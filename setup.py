a06ca9767c5c481142fbb284898c5da3aba10a2c
David Cameron <dave.j.cameron@gmail.com>
2017-04-21 14:59:53 -0400

    CHG: Updated license and pypi password

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
         'Intended Audience :: Developers',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: Apache 2',
+        'License :: OSI Approved :: Apache Software License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
