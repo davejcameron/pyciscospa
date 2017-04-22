9c47afbf1fd71341feeacd2528bb10fd297a67dd
David Cameron <dave.j.cameron@gmail.com>
2017-04-22 08:01:38 -0400

    FIX: Deploy to PyPi

--- ci/travis_set_build.py
+++ ci/travis_set_build.py
@@ -1,24 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Update the version string with a build number."""
-
-from __future__ import absolute_import
-from __future__ import print_function
+"""Append a build number to a version string. See Pep 440."""
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals, with_statement)
 
 import io
+import logging
 import os
 import re
-
 
 PROJECT_ROOT = os.path.abspath(os.path.join(
     os.path.dirname(os.path.abspath(__file__)),
     os.pardir)
 )
+VERSION_FILE_PATHS = ('src', 'pyciscospa', '__init__.py')
+VERSION_FILE = os.path.join(PROJECT_ROOT, *VERSION_FILE_PATHS)
 BUILD_NUMBER = os.environ.get('TRAVIS_BUILD_NUMBER', '')
-BUILD_TYPE = 'dev'  # See PEP 440
+
+logformat = '[%(name)s]: %(message)s'
+logging.basicConfig(format=logformat, level=logging.INFO)
+log = logging.getLogger('travis_set_build')
 
 
 def replace(file_path, pattern, repl, flags=0):
+    """Replace a pattern in a file."""
     with io.open(file_path, mode="r+", newline='') as fh_:
         file_contents = fh_.read()
         file_contents = re.sub(pattern, repl, file_contents, flags=flags)
@@ -27,32 +32,32 @@ def replace(file_path, pattern, repl, flags=0):
         fh_.write(file_contents)
 
 
-def append_version(build, file_paths):
+def append_build(build, version_file):
+    """Append a build number to a version string in a file."""
     # The version line must have the form
     # __version__ = 'ver'
     pattern = r"^(__version__ = ['\"])([^'\"]*)(['\"])"
-    repl = r"\g<1>\g<2>.{0}\g<3>".format(build)
-    version_file = os.path.join(PROJECT_ROOT, *file_paths)
-    print(
-        'Updating version in version_file "{0}" with build "{1}"'
-        .format(version_file, build)
+    repl = r"\g<1>\g<2>{0}\g<3>".format(build)
+    log.info(
+        'Updating version in version_file "%s" with build "%s"',
+        version_file, build
     )
     replace(version_file, pattern, repl, flags=re.M)
 
 
 def main(args):
+    """Process args and set version."""
     skip = args.skip
     build = args.build
-    build_type = args.build_type
-    file_paths = args.file_paths
+    version_file = args.version_file
 
     if skip:
-        print(
-            'Not updating version for this build, `skip` set to "{0}"'
-            .format(skip)
+        log.info(
+            'Not updating version for this build, `skip` set to "%s"',
+            skip
         )
     else:
-        append_version('{0}{1}'.format(build_type, build), file_paths)
+        append_build(build, version_file)
 
 
 if '__main__' == __name__:
@@ -76,20 +81,11 @@ if '__main__' == __name__:
         )
     )
     parser.add_argument(
-        '--build-type',
-        default=BUILD_TYPE,
+        '--version-file',
+        default=VERSION_FILE,
         help=(
-            'Build type to append to the version. See PEP 440. PyPi will not '
-            'accept non-compliant version identifiers (default: {0})'
-            .format(BUILD_TYPE)
-        )
-    )
-    parser.add_argument(
-        '--file-paths',
-        required=True,
-        help=(
-            'Tuple of paths relative to the project root to a file containing '
-            'the version string.'
+            'Path to the file containing the version string. '
+            '(default: {0})'.format(VERSION_FILE)
         )
     )
 
